9b59b60d40b6534c69337d88ec0f8551e59fd01a
henease <wenghengc@gmail.com>
2016-11-17 22:30:11 +0800

    change to BigDecimal

--- src/main/java/com/netease/web/controller/Show.java
+++ src/main/java/com/netease/web/controller/Show.java
@@ -1,7 +1,7 @@
 package com.netease.web.controller;
 
 import java.io.UnsupportedEncodingException;
-
+import java.math.BigDecimal;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -26,10 +26,14 @@ public class Show {
 		Product product = myService.getProduct(id);
 		String detail = new String(product.getDetail().getBytes("iso-8859-1"),"UTF-8");
 		product.setDetail(detail);
-		product.setPrice((double)(product.getPrice())/100);
+		//product.setPrice((double)(product.getPrice())/100);
+		product.setPrice(new BigDecimal(Double.toString(product.getPrice()))
+				.divide(new BigDecimal(Integer.toString(100))).doubleValue());
 		if(product.getBuyPrice() != null){
 			product.setIsBuy("true");
-			product.setBuyPrice((double)(product.getBuyPrice())/100);
+			//product.setBuyPrice((double)(product.getBuyPrice())/100);
+			product.setBuyPrice(new BigDecimal(Double.toString(product.getBuyPrice()))
+					.divide(new BigDecimal(Integer.toString(100))).doubleValue());
 		}
 		model.addAttribute(product);
 		return "show";

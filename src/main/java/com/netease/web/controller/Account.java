080b0b2a682a457bd0d4022f8a09bf8204cf9415
henease <wenghengc@gmail.com>
2016-11-19 16:06:14 +0800

    add comments & util.PriceTrans

--- src/main/java/com/netease/web/controller/Account.java
+++ src/main/java/com/netease/web/controller/Account.java
@@ -2,7 +2,7 @@ package com.netease.web.controller;
 
 import java.util.Iterator;
 import java.util.List;
-import java.math.BigDecimal;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -15,12 +15,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.netease.web.meta.Product;
 import com.netease.web.serviceimpl.MyServiceImpl;
 import com.netease.web.utils.CheckSession;
+import com.netease.web.utils.PriceTrans;
 
+//该Controller处理账务的数据
 @Controller
 public class Account {
+	//注入MyServiceImpl
 	@Autowired
 	private MyServiceImpl myService;
 	
+	/* 利用checkUser方法验证登录用户
+	 * 利用getTrx()方法获取trx表中的product数据，传入List
+	 * 对每个从数据库中取出的price进行浮点数转换
+	 */
 	@RequestMapping(path="/account",method=RequestMethod.GET)
 	public String account(HttpServletRequest req,HttpSession session,Model model){
 		CheckSession.checkUser(session, model, req);
@@ -28,9 +35,7 @@ public class Account {
 		Iterator<Product> it = productList.iterator();
 		while(it.hasNext()){
 			Product product = it.next();
-			//product.setBuyPrice((double)(product.getBuyPrice())/100);
-			product.setBuyPrice(new BigDecimal(Double.toString(product.getBuyPrice()))
-					.divide(new BigDecimal(Integer.toString(100))).doubleValue());
+			product.setBuyPrice(PriceTrans.transPriceToDouble(product.getBuyPrice()));
 		}
 		model.addAttribute(productList);
 		return "account";

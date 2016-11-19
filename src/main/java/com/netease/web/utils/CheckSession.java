6e7832884f2e9f8802139613293fec4bf8bfa8f9
henease <wenghengc@gmail.com>
2016-11-19 16:15:50 +0800

    add comments

--- src/main/java/com/netease/web/utils/CheckSession.java
+++ src/main/java/com/netease/web/utils/CheckSession.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import com.netease.web.meta.User;
 
 public class CheckSession {
+	//获取请求中的session，验证是否含有user信息，判断buyer或seller是否登录
 	public static void checkUser(HttpSession session,Model model,HttpServletRequest req){
 		session = req.getSession();
 		if(session.getAttribute("currentUser") != null){

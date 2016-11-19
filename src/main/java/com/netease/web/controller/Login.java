a38f4f2d210a242e2d71fbeb221e8a54823a68b5
henease <wenghengc@gmail.com>
2016-11-19 16:09:59 +0800

    add comments

--- src/main/java/com/netease/web/controller/Login.java
+++ src/main/java/com/netease/web/controller/Login.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+//该Controller处理登录页面
 @Controller
 public class Login {
 	
+	//不需要传入数据，直接返回login字符串
 	@RequestMapping(path="/login",method=RequestMethod.GET)
 	public String login(){
 		return "login";

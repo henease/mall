0d8d3973976b54149e810bee47df831685013c30
henease <wenghengc@gmail.com>
2016-11-19 16:11:11 +0800

    add comments

--- src/main/java/com/netease/web/controller/Pub.java
+++ src/main/java/com/netease/web/controller/Pub.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.netease.web.utils.CheckSession;
 
+//该Controller处理发布页面
 @Controller
 public class Pub {
+	
+	//该页面不需要传入数据，只需要验证用户登录后返回public字符串
 	@RequestMapping(path="/public",method=RequestMethod.GET)
 	public String pub(HttpSession session,HttpServletRequest req,Model model){
 		CheckSession.checkUser(session, model, req);

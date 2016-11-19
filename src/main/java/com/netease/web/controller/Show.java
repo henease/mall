94f3212bb9aeb2591f982533714d76c946a74cd0
henease <wenghengc@gmail.com>
2016-11-19 16:12:23 +0800

    add comments & util.PriceTrans

--- src/main/java/com/netease/web/controller/Show.java
+++ src/main/java/com/netease/web/controller/Show.java
@@ -1,7 +1,7 @@
 package com.netease.web.controller;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigDecimal;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -14,26 +14,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.netease.web.meta.Product;
 import com.netease.web.serviceimpl.MyServiceImpl;
 import com.netease.web.utils.CheckSession;
+import com.netease.web.utils.PriceTrans;
 
+//该Controller处理show页面的数据
 @Controller
 public class Show {
+	//自动注入MyServiceImpl
 	@Autowired
 	private MyServiceImpl myService;
 	
+	/* 处理show页面的数据
+	 * checkUser验证登录用户；
+	 * 根据请求时的id参数从数据库获取对应的product对象；
+	 * 对detail属性进行编码转换，避免乱码，对price进行double转换；
+	 * 通过left join同时从content和trx表获取数据，
+	 * 如果有buyPrice属性值，则设置isBuy，对trx表中的buyPrice进行double转换
+	 */
 	@RequestMapping(path="/show",method=RequestMethod.GET)
-	public String show(HttpServletRequest req,HttpSession session,Model model,@RequestParam("id")int id) throws UnsupportedEncodingException{
+	public String show(HttpServletRequest req,HttpSession session,Model model,@RequestParam("id")int id){
 		CheckSession.checkUser(session, model, req);
 		Product product = myService.getProduct(id);
-		String detail = new String(product.getDetail().getBytes("iso-8859-1"),"UTF-8");
-		product.setDetail(detail);
-		//product.setPrice((double)(product.getPrice())/100);
-		product.setPrice(new BigDecimal(Double.toString(product.getPrice()))
-				.divide(new BigDecimal(Integer.toString(100))).doubleValue());
+		try{
+			String detail = new String(product.getDetail().getBytes("iso-8859-1"),"UTF-8");
+			product.setDetail(detail);
+		}catch(UnsupportedEncodingException e){
+			e.printStackTrace();
+		}
+		product.setPrice(PriceTrans.transPriceToDouble(product.getPrice()));
 		if(product.getBuyPrice() != null){
 			product.setIsBuy("true");
-			//product.setBuyPrice((double)(product.getBuyPrice())/100);
-			product.setBuyPrice(new BigDecimal(Double.toString(product.getBuyPrice()))
-					.divide(new BigDecimal(Integer.toString(100))).doubleValue());
+			product.setBuyPrice(PriceTrans.transPriceToDouble(product.getBuyPrice()));
 		}
 		model.addAttribute(product);
 		return "show";

f0a8e923ac7f96863b8adb37eb3d0c5b02753c79
henease <wenghengc@gmail.com>
2016-11-19 16:10:34 +0800

    add comments

--- src/main/java/com/netease/web/controller/Logout.java
+++ src/main/java/com/netease/web/controller/Logout.java
@@ -7,8 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//该Controller处理退出页面
 @Controller
 public class Logout {
+	
+	/* 处理退出页面，获取session，验证session是否为空
+	 * 为空则利用invalidate方法清空信息，跳转至login页面
+	 */
 	@RequestMapping(path="/logout",method=RequestMethod.GET)
 	public String logout(HttpServletRequest req,HttpSession session){
 		session = req.getSession();
